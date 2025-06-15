3d14914ab8c0fe8ef09af0071f907274df60a9b7
jesusking2722 <jesusking2722@gmail.com>
2025-06-15 20:29:57 +0200

    feat: add SearchInput component to components/molecules

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -2,4 +2,5 @@ export { default as AccordionGroup } from "./AccordionGroup";
 export { default as HeaderSearch } from "./HeaderSearch";
 export { default as ProcessStep } from "./ProcessStep";
 export { default as ProjectCard } from "./ProjectCard";
+export { default as SearchInput } from "./SearchInput";
 export { default as ServiceCategoryCard } from "./ServiceCategoryCard";
